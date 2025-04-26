1fc509e3ebb5dabeffd2b3e6cb195217a0453fd6
yemyk <egor2004mikh@gmail.com>
2025-04-26 11:51:16 +0200

    Initial Commit

---
+++ ClothingApp/ClothingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClothingAppApp.swift
+//  ClothingApp
+//
+//  Created by yehor on 26.04.25.
+//
+
+import SwiftUI
+
+@main
+struct ClothingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
